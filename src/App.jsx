4fc9cee6ddacf88e74960fe7fe9ab6eb7f9d2d76
tamirakhuu <tamirakhuu@gmail.com>
2026-09-22 17:51:33 +0800

    d

--- src/App.jsx
+++ src/App.jsx
@@ -949,7 +949,7 @@ function ProductDetail({ product, onBack, onAddToCart, onQuickAdd, isWished, onT
             <div style={{ fontFamily: "'Ubuntu', sans-serif", fontSize: 12.5, color: T.moss, marginBottom: 20 }}>
               {qty >= bulkBoxQty
                 ? ` ${bulkBoxQty}+ ширхэгт бөөний үнээр тооцогдож байна`
-                : `${bulkBoxQty} ширхэг буюу хайрцагаар нь авбал бөөний үнээр тооцно`}
+                : `${bulkBoxQty} ширхэг болон хайрцагаар нь авбал бөөний үнээр тооцно`}
             </div>
           )}
 
